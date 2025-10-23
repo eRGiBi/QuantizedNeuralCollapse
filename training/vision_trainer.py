7e1bd695f3ffcd3c9ff11209b54fb3e7bf445dbe
eRGiBi <xx4qwexx@gmail.com>
2025-10-23 02:41:00 +0200

    Create vision_trainer.py

---
+++ training/vision_trainer.py
@@ -0,0 +1,22 @@
+from training.base_trainer import BaseModelTrainer
+
+
+class CVModelTrainer(BaseModelTrainer):
+    """Trainer for computer vision models."""
+
+    def _process_batch(self, batch_data):
+        """Process a batch for computer vision models."""
+        inputs, labels = batch_data
+        inputs, labels = inputs.to(self.device), labels.to(self.device)
+
+        outputs = self.model(inputs)
+        return outputs, labels
+
+    def _compute_loss_and_accuracy(self, outputs, labels, criterion):
+        """Compute loss and accuracy for CV models."""
+        loss = criterion(outputs, labels)
+        _, predicted = outputs.max(1)
+        correct = predicted.eq(labels).sum().item()
+        total = labels.size(0)
+
+        return loss, correct, total
