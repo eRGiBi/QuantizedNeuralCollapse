75d0d9f14cdd17d320693277875ecc6aca8071bd
eRGiBi <xx4qwexx@gmail.com>
2025-12-01 01:49:20 +0100

    Refactoring

--- training/vision_trainer.py
+++ training/vision_trainer.py
@@ -4,6 +4,10 @@ from training.base_trainer import BaseModelTrainer
 class VisionTrainer(BaseModelTrainer):
     """Trainer for computer vision models."""
 
+    # def _process_batch_cv(self, inputs, labels):
+    #     outputs = self.model(inputs)
+    #     return outputs, labels
+
     def _process_batch(self, batch_data):
         """Process a batch for computer vision models."""
         inputs, labels = batch_data
@@ -11,6 +15,7 @@ class VisionTrainer(BaseModelTrainer):
 
         outputs = self.model(inputs)
         return outputs, labels
+
 
     def _compute_loss_and_accuracy(self, outputs, labels, criterion):
         """Compute loss and accuracy for CV models."""
@@ -20,19 +25,3 @@ class VisionTrainer(BaseModelTrainer):
         total = labels.size(0)
 
         return loss, correct, total
-
-
-def _process_batch_cv(self, inputs, labels):
-    """Process a batch for computer vision models."""
-    outputs = self.model(inputs)
-    return outputs, labels
-
-
-def _compute_loss_and_accuracy_cv(self, outputs, labels, criterion):
-    """Compute loss and accuracy for CV models."""
-    loss = criterion(outputs, labels)
-    _, predicted = outputs.max(1)
-    correct = predicted.eq(labels).sum().item()
-    total = labels.size(0)
-
-    return loss, correct, total
